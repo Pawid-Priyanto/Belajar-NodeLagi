f9820e2d1aff4c24f4b29b592062c982957a7f7b
Pawid-Priyanto <pawitanto27@gmail.com>
2019-11-13 13:55:21 +0700

    update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Ini tampilan web'))
+app.get('/profile', (req, res) => res.send('Ini tampilan profile'))
+app.get('/aboutus', (req, res) => res.send('Ini tampilan abou us'))
 
 app.listen(5000, ()=> console.log("Project pertama saya"))
